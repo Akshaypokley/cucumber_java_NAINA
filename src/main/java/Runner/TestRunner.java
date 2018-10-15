63b061c5640cab41def78e73ae5f512fa2e4d317
akshay.pokley <akshay85pokley@gmail.com>
2018-10-15 17:43:56 +0530

    Study about below topics tags , datata tabels with raw method and map method

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -1,10 +1,17 @@
 package Runner;
 
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 
 
+import cucumber.api.Scenario;
 import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Created by akshay.pokley on 10/11/2018.
@@ -12,8 +19,9 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
+
 @CucumberOptions(
-        features = "E:\\Akshay\\SELENIUM\\Projects\\cucumber_java_NAINA\\src\\main\\java\\Features\\HooksConcept.feature",
+        features = "E:\\Akshay\\SELENIUM\\Projects\\cucumber_java_NAINA\\src\\main\\java\\Features\\CreateProsDtatable.feature",
         glue = {"StepDefination"},
       //  tags = {"~@Ignore"},
     //  dryRun = true,/*Cheking mapping betwemnn feature and steps defination ,Which method is missing in step defination */
@@ -22,7 +30,8 @@ dryRun = false,/*browse lauching  */
         //strict = true,/*excuting all properly mapping steps ,it will ceck any steps defination is not avalable */
 //tags = {"@RegressionTest,@EndToEndTest"}, /*, is a or oprator*/
        tags = {"~@RegressionTest","~@SmokeTest"}, /*For ignoring any perticular secnario then used '~'*/
-
+        plugin = {
+                "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html" },
 
         format = {
                 "pretty",
@@ -34,6 +43,27 @@ dryRun = false,/*browse lauching  */
 
 
 public class TestRunner {
+
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        Reporter.assignAuthor("ToolsQA - AKSHAY ");
+        if (scenario.getName().equals("Some scenario name")) {
+            Reporter.assignAuthor("ToolsQA - AKSHAY");
+        }
+    }
+
+
+
+    @AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig("E:\\Akshay\\SELENIUM\\Projects\\cucumber_java_NAINA\\src\\main\\java\\config\\extent-config.xml");
+        Reporter.setSystemInfo("Machine", 	"Windows 10" + "64 Bit");
+        Reporter.setSystemInfo("Selenium", "3.7.0");
+        Reporter.setSystemInfo("Maven", "3.5.2");
+        Reporter.setSystemInfo("Java Version", "1.8.0_151");
+
+    }
+
 
 
 }
---
+++ src/main/java/Runner/old/TestRunnerCopy.java
@@ -0,0 +1,39 @@
+package Runner;
+
+import cucumber.api.CucumberOptions;
+
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by akshay.pokley on 10/11/2018.
+ */
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "E:\\Akshay\\SELENIUM\\Projects\\cucumber_java_NAINA\\src\\main\\java\\Features\\HooksConcept.feature",
+        glue = {"StepDefination"},
+      //  tags = {"~@Ignore"},
+    //  dryRun = true,/*Cheking mapping betwemnn feature and steps defination ,Which method is missing in step defination */
+dryRun = false,/*browse lauching  */
+      //  monochrome = true,/*it will genrate the output in redabel fromat*/
+        //strict = true,/*excuting all properly mapping steps ,it will ceck any steps defination is not avalable */
+//tags = {"@RegressionTest,@EndToEndTest"}, /*, is a or oprator*/
+       tags = {"~@RegressionTest","~@SmokeTest"}, /*For ignoring any perticular secnario then used '~'*/
+
+
+        format = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt",
+                "junit:target/cucumber-reports/Junittest.xml"
+        })
+
+
+public class TestRunnerCopy {
+
+
+}
